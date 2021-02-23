f6e9802a0d6d58380b8ade792f1b635eea24fd2a
tongsumate <sumet.srithong@gmail.com>
2021-02-23 12:33:42 +0700

    join

--- Character.py
+++ Character.py
@@ -5,8 +5,8 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-#client = pymongo.MongoClient("mongodb://admin:AAGzez02811@10.100.2.126:27017")
-client = pymongo.MongoClient("mongodb://admin:AAGzez02811@node9149-advweb-11.app.ruk-com.cloud:11154")
+client = pymongo.MongoClient("mongodb://admin:AAGzez02811@10.100.2.126:27017")
+#client = pymongo.MongoClient("mongodb://admin:AAGzez02811@node9149-advweb-11.app.ruk-com.cloud:11154")
 
 db = client["MMORPG"]
 
@@ -163,4 +163,4 @@ def delete_character(name):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port = 5000)
+    app.run(host='0.0.0.0',port = 80)
